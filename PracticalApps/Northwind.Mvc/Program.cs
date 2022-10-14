7256724ceff5288941b46772e8dc7179f74e968f
dimawest <107949610+dimawes@users.noreply.github.com>
2022-10-14 14:03:57 +0300

    cleaning

--- PracticalApps/Northwind.Mvc/Program.cs
+++ PracticalApps/Northwind.Mvc/Program.cs
@@ -1,6 +1,6 @@
-using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
-using Northwind.Mvc.Data;
+using Microsoft.AspNetCore.Identity; // IdentityUser
+using Microsoft.EntityFrameworkCore; // UseSqlServer, UseSqlite
+using Northwind.Mvc.Data; // ApplicationDbContext
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -24,7 +24,9 @@ if (app.Environment.IsDevelopment())
 else
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    // The default HSTS value is 30 days.
+    // You may want to change this for production scenarios,
+    // see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
